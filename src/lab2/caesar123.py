28dddd2ce40832449c1a2ec221847c5e1f30892a
сережа 🧟‍♂️ <glavnipopivy@MacBook-Air-sereza-2.local>
2024-10-02 00:33:07 +0300

    lastfuck1337

--- src/lab2/caesar123.py
+++ src/lab2/caesar123.py
@@ -1,55 +1,62 @@
-import typing as tp
+from operator import lshift
 
 
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+def encrypt_caesar(plaintext):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
-
     for char in plaintext:
-        if char.isalpha():
-            if char.isupper():
-                prom = ord('A')
-            else:
-                prom = ord('a')
-            new_symb = chr((ord(char) - prom + shift) % 26 + prom)
-            ciphertext += new_symb
+        if char.isalpha():  
+            shift_amount = lshift % 26  
+            """
+            Проверяем, является ли символ буквой и гарантируем, что сдвиг находится в пределах алфавита
+            """
+            if char.isupper():  # для заглавных букв
+                shifted_char = chr(((ord(char) - ord('A') + shift_amount) % 26) + ord('A'))
+            else:  # для строчных букв
+                shifted_char = chr(((ord(char) - ord('a') + shift_amount) % 26) + ord('a'))
         else:
-            ciphertext += char
-
+            shifted_char = char  # не изменяем не-буквенные символы
+        ciphertext += shifted_char
     return ciphertext
 
 
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+
+def decrypt_caesar(ciphertext):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
-
     for char in ciphertext:
-        if char.isalpha():
-            if char.isupper():
-                prom = ord('A')
-            else:
-                prom = ord('a')
-            new_symb = chr((ord(char) - prom - shift) % 26 + prom)
-            plaintext += new_symb
+        if char.isalpha():  
+            shift_amount = lshift % 26  
+            """
+            Проверяем, является ли символ буквой и гарантируем, что сдвиг находится в пределах алфавита
+            """
+            if char.isupper():  # для заглавных букв
+                shifted_char = chr(((ord(char) - ord('A') - shift_amount) % 26) + ord('A'))
+            else:  # для строчных букв
+                shifted_char = chr(((ord(char) - ord('a') - shift_amount) % 26) + ord('a'))
         else:
-            plaintext += char
-
+            shifted_char = char  
+            """
+            Если символы не являются буквами - не изменяем их
+            """
+        plaintext += shifted_char
     return plaintext
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
-
-
-
-input_text = "PyTHon3.6"
-ciphertext = encrypt_caesar(input_text)
-print(ciphertext)
-
-
-input_text = "SbWKrq3.6"
-plaintext = decrypt_caesar(input_text)
-print(plaintext)
