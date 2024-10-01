e0123ce9d2a86cf32518726e74f32dcde6f05ec8
сережа 🧟‍♂️ <glavnipopivy@MacBook-Air-sereza-2.local>
2024-10-01 23:43:11 +0300

    Test12

--- src/lab2/vigenere123.py
+++ src/lab2/vigenere123.py
@@ -9,22 +9,25 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    plaintext = plaintext.upper()
     keyword = keyword.upper()
     ciphertext = []
     keyword_repeated = (keyword * (len(plaintext) // len(keyword) + 1))[:len(plaintext)]
 
     keyword_index = 0
     for p in plaintext:
-        if p.isalpha():  
+        if p.isalpha():
             shift = ord(keyword_repeated[keyword_index]) - ord('A')
-            encrypted_char = chr((ord(p) - ord('A') + shift) % 26 + ord('A'))
+            if p.isupper():
+                encrypted_char = chr((ord(p) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                encrypted_char = chr((ord(p) - ord('a') + shift) % 26 + ord('a'))
             ciphertext.append(encrypted_char)
-            keyword_index += 1  
+            keyword_index += 1
         else:
             ciphertext.append(p)
 
     return ''.join(ciphertext)
+
 
 def decrypt_vigenere(ciphertext, keyword):
     """
@@ -37,18 +40,20 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    ciphertext = ciphertext.upper()
     keyword = keyword.upper()
     plaintext = []
     keyword_repeated = (keyword * (len(ciphertext) // len(keyword) + 1))[:len(ciphertext)]
 
     keyword_index = 0
     for c in ciphertext:
-        if c.isalpha():  
+        if c.isalpha():
             shift = ord(keyword_repeated[keyword_index]) - ord('A')
-            decrypted_char = chr((ord(c) - ord('A') - shift) % 26 + ord('A'))
+            if c.isupper():
+                decrypted_char = chr((ord(c) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                decrypted_char = chr((ord(c) - ord('a') - shift) % 26 + ord('a'))
             plaintext.append(decrypted_char)
-            keyword_index += 1 
+            keyword_index += 1
         else:
             plaintext.append(c)
 
